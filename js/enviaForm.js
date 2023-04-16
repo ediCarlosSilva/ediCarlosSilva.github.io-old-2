b2b6f113b6c1059bb6cf5015566dfbf90c2f17f2
Edi Carlos <edi_csilva@hotmail.com>
2023-04-16 20:11:33 +0000

    ajuste no pattern do e-mail e formulario

--- js/enviaForm.js
+++ js/enviaForm.js
@@ -21,31 +21,31 @@ async function handleSubmit(event) {
 
     // console.log(data);
 
-    // fetch('https://formspree.io/f/xrgvozjb', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(data)
-    // }).then(response => {
-    //     if (response.ok) {
-    //         statusBotao.innerHTML = `Obrigado ${data.name}. Retornarei o mais rápido possível.`
-    //         statusBotao.style.maxWidth = "100%"
-    //         statusBotao.classList.add('formcontato__mensagemEnviada');
-    //         form.reset();
-    //         setTimeout(retornaCorBotao, 10000);
-    //     } else {
-    //         response.json().then(data => {
-    //             if (Object.hasOwn(data, 'errors')) {
-    //                 statusBotao.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-    //             } else {
-    //                 statusBotao.innerHTML = "Oops! There was a problem submitting your form"
-    //             }
-    //         })
-    //     }
-    // }).catch(error => {
-    //     statusBotao.innerHTML = "Oops! There was a problem submitting your form"
-    // });
+    fetch('https://formspree.io/f/xrgvozjb', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(response => {
+        if (response.ok) {
+            statusBotao.innerHTML = `Obrigado ${data.name}. Retornarei o mais rápido possível.`
+            statusBotao.style.maxWidth = "100%"
+            statusBotao.classList.add('formcontato__mensagemEnviada');
+            form.reset();
+            setTimeout(retornaCorBotao, 10000);
+        } else {
+            response.json().then(data => {
+                if (Object.hasOwn(data, 'errors')) {
+                    statusBotao.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                } else {
+                    statusBotao.innerHTML = "Oops! There was a problem submitting your form"
+                }
+            })
+        }
+    }).catch(error => {
+        statusBotao.innerHTML = "Oops! There was a problem submitting your form"
+    });
 
 }
 
